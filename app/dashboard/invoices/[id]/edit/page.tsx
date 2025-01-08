cfff531533b7b55e3c92fa3ab50de25b9c660812
tranthientam293 <tranthientam2903@gmail.com>
2025-01-08 11:00:17 +0700

    add functions

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,36 @@
+import Form from "@/app/ui/invoices/edit-form"
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs"
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data"
+import { notFound } from "next/navigation"
+
+interface Props {
+  params: Promise<{ id: string }>
+}
+export default async function Page(props: Props) {
+  const params = await props.params
+  const id = params.id
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ])
+
+  if (!invoice) {
+    notFound()
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  )
+}
